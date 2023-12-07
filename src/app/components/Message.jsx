4f1816a8f25185f2f52081c53ef7c631bf8dcd5f
sameemul-haque <samhaqk@gmail.com>
2023-12-07 22:23:12 +0530

    📋 Add copy button to Message component

--- src/app/components/Message.jsx
+++ src/app/components/Message.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Message = ({ message }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(message.text);
+    setIsCopied(true);
+  };
+
   return (
     <div className="chat-bubble">
       <img className="chat-img" src={message.avatar} alt="user avatar" />
       <p className="user-message">{message.text}</p>
+      <button className="copy-button" onClick={handleCopy} disabled={isCopied}>
+        {isCopied ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="12"
+            viewBox="0 0 384 512"
+            fill="#a6e3a1"
+          >
+            <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM305 273L177 401c-9.4 9.4-24.6 9.4-33.9 0L79 337c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L271 239c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="12"
+            viewBox="0 0 384 512"
+            fill="#a6e3a1"
+          >
+            <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+          </svg>
+        )}
+      </button>
     </div>
   );
 };
