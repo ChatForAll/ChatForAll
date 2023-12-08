a5f0ca448093ee21c82a4e2b2c919f9523342be5
sameemul-haque <samhaqk@gmail.com>
2023-12-08 22:21:05 +0530

    ✅ Update copy button updated svg for both buttons add time to reset the button

--- src/app/components/Message.jsx
+++ src/app/components/Message.jsx
@@ -6,6 +6,9 @@ const Message = ({ message }) => {
   const handleCopy = () => {
     navigator.clipboard.writeText(message.text);
     setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 3000);
   };
 
   return (
@@ -15,23 +18,23 @@ const Message = ({ message }) => {
       <button className="copy-button" onClick={handleCopy} disabled={isCopied}>
         {isCopied ? (
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="16"
-            width="12"
-            viewBox="0 0 384 512"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
             fill="#a6e3a1"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM305 273L177 401c-9.4 9.4-24.6 9.4-33.9 0L79 337c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L271 239c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+            <path d="M18.063 5.674a1 1 0 0 1 .263 1.39l-7.5 11a1 1 0 0 1-1.533.143l-4.5-4.5a1 1 0 1 1 1.414-1.414l3.647 3.646 6.82-10.002a1 1 0 0 1 1.39-.263Z" />
           </svg>
         ) : (
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="16"
-            width="12"
-            viewBox="0 0 384 512"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
             fill="#a6e3a1"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM112 192H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+            <path d="M12 4a2 2 0 0 0-2 2h4a2 2 0 0 0-2-2ZM8.535 4A3.998 3.998 0 0 1 12 2c1.48 0 2.773.804 3.465 2H17a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3h1.535ZM8 6H7a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-1a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2Z" />
           </svg>
         )}
       </button>
