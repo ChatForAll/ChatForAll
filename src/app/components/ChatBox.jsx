5d0c465a16b1389250d41a7ac56b4410502aa491
sameemul-haque <samhaqk@gmail.com>
2023-12-19 00:35:30 +0530

    ⌛ Add Preloader component

--- src/app/components/ChatBox.jsx
+++ src/app/components/ChatBox.jsx
@@ -12,10 +12,12 @@ import { db } from "../../firebase-config";
 import AdminMessage from "./AdminMessage";
 import Message from "./Message";
 import SendMessage from "./SendMessage";
+import Preloader from "./Preloader";
 
 const ChatBox = () => {
   const [messages, setMessages] = useState([]);
   const scroll = useRef();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
@@ -29,6 +31,7 @@ const ChatBox = () => {
         (a, b) => a.createdAt - b.createdAt
       );
       setMessages(sortedMessages);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
@@ -52,16 +55,21 @@ const ChatBox = () => {
 
   return (
     <>
-      <div className="p-5 mb-16 text-[#4c4f69] dark:text-[#cad3f5]">
-        <AdminMessage>
-          Welcome to ChatForAll. Enjoy hassle-free chatting without
-          authentication. Easily copy messages using the dedicated button.
-          Messages auto-remove after 48 hours. Happy chatting 🚀
-        </AdminMessage>
-        {messages?.map((message) => (
-          <Message key={message.id} message={message} />
-        ))}
-      </div>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <div className="p-5 mb-16 text-[#4c4f69] dark:text-[#cad3f5]">
+          <AdminMessage>
+            Greetings and welcome to ChatForAll, a simple online chat
+            application. There is no need to log in. Use the clipboard button to
+            quickly copy the messages. Messages that are older than two days
+            will be automatically removed. Happy Chatting🚀
+          </AdminMessage>
+          {messages?.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+        </div>
+      )}
       {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
       <span ref={scroll}></span>
       <SendMessage scroll={scroll} />
---
+++ src/app/components/Preloader.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+const Preloader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <Image
+        alt="loading..."
+        src="loading.svg"
+        // src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiBzdHlsZT0ibWFyZ2luOiBhdXRvOyBiYWNrZ3JvdW5kOiBub25lOyBkaXNwbGF5OiBibG9jazsgc2hhcGUtcmVuZGVyaW5nOiBhdXRvOyIgd2lkdGg9IjI4NXB4IiBoZWlnaHQ9IjI4NXB4IiB2aWV3Qm94PSIwIDAgMTAwIDEwMCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ieE1pZFlNaWQiPgo8Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiBmaWxsPSJub25lIiBzdHJva2U9IiM2MTc2ZGMiIHN0cm9rZS13aWR0aD0iMyIgcj0iMjEiIHN0cm9rZS1kYXNoYXJyYXk9Ijk4Ljk2MDE2ODU4ODA3ODQ5IDM0Ljk4NjcyMjg2MjY5MjgzIj4KICA8YW5pbWF0ZVRyYW5zZm9ybSBhdHRyaWJ1dGVOYW1lPSJ0cmFuc2Zvcm0iIHR5cGU9InJvdGF0ZSIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiIGR1cj0iMXMiIHZhbHVlcz0iMCA1MCA1MDszNjAgNTAgNTAiIGtleVRpbWVzPSIwOzEiPjwvYW5pbWF0ZVRyYW5zZm9ybT4KPC9jaXJjbGU+CjwhLS0gW2xkaW9dIGdlbmVyYXRlZCBieSBodHRwczovL2xvYWRpbmcuaW8vIC0tPjwvc3ZnPg=="
+      />
+    </div>
+  );
+};
+
+export default Preloader;
