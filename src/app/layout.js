d7fd32faca1c134809dec245c965b1282fd7c369
sameemul-haque <samhaqk@gmail.com>
2024-05-07 20:09:45 +0530

    chore: pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,18 @@ export const metadata = {
   title: "ChatForAll",
   description:
     "A simple realtime chat applcation without authentication created using Next.js and Firebase",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Sameemul Haque" },
+    {
+      name: "Sameemul Haque",
+      url: "https://sameemul-haque.github.io/",
+    },
+  ],
+  viewport:
+  "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
 };
 
 export default function RootLayout({ children }) {
